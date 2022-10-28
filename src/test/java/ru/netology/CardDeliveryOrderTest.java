679250c163c4e517303b3031dc9e2f4c52e36af6
mashakulina <masha_kulina@mail.ru>
2022-10-28 21:20:52 +0300

    changes have been made to CardDeliveryOrderTest.java and build.gradle

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -1,43 +1,61 @@
 package ru.netology;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.DataGenerator.getCorrectDate;
+import static ru.netology.DataGenerator.getIncorrectDate;
 
 public class CardDeliveryOrderTest {
+    private final String date = getCorrectDate(7);
 
-    private String stringDate;
-
-    @BeforeEach
-    void dateToBeSet() {
-        LocalDate today = LocalDate.now();
-        LocalDate dateToBeSet = today.plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        stringDate = dateToBeSet.format(formatter);
-    }
+    private final String invalidDate = getIncorrectDate();
 
     @Test
-    void test() {
-
-        Configuration.holdBrowserOpen = true;
+    void shouldBeReservation() {
 
         open("http://localhost:9999");
         $$x("//input[@type='text']").get(0).setValue("Москва");
-        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $x("//input[@placeholder='Дата встречи']").setValue(stringDate);
+        $x("//input[@placeholder='Дата встречи']").click();
         $x("//input[@name='name']").setValue("Иван Иванов");
         $("[name='phone']").setValue("+79672699504");
         $("[data-test-id='agreement']").click();
         $("[class='button__text']").click();
         $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
-
     }
+    @Test
+    void shouldBeReservationOnNewDate() {
+
+        open("http://localhost:9999");
+        $$x("//input[@type='text']").get(0).setValue("Москва");
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $x("//input[@placeholder='Дата встречи']").setValue(date);
+        $x("//input[@name='name']").setValue("Иван Иванов");
+        $("[name='phone']").setValue("+79672699504");
+        $("[data-test-id='agreement']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void shouldBeNotReservationOnInvalidDate() {
+
+        open("http://localhost:9999");
+        $$x("//input[@type='text']").get(0).setValue("Москва");
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $x("//input[@placeholder='Дата встречи']").setValue(invalidDate);
+        $x("//input[@name='name']").setValue("Иван Иванов");
+        $("[name='phone']").setValue("+79672699504");
+        $("[data-test-id='agreement']").click();
+        $("[class='button__text']").click();
+        $(byText("Заказ на выбранную дату невозможен")).should(visible, Duration.ofSeconds(15));
+    }
+
+
+
 }
